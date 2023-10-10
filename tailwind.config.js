70530ad9241a705522b004330b46ad69656b4acf
nono <nono.polkadot@gmail.com>
2023-10-10 15:27:29 +0800

    slicing hingga service section

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "inter" : ['inter'],
+        "Circular" : ['Circular STD']
+      },
+      colors: {
+        "ungu" : '#5D50C6',
+        "merah-muda" : "#F85E9F",
+        "oren" : "#FF5722",
+        'abu' : "#222831"
+      },
+      dropShadow : {
+        "sm-shadow" : [
+          '0px 548px 219px rgba(0, 0, 0, 0, 0.01)',
+          '0px 308px 185px rgba(0, 0, 0, 0, 0.04)',
+          '0px 137px 137px rgba(0, 0, 0, 0, 0.06)',
+          '0px 34px 75px rgba(0, 0, 0, 0, 0.07)',
+          '0px 0px 0px rgba(0, 0, 0, 0, 0.07)',
+        ],
+        "lg-shadow" : [
+          '0px 81px 32px rgba(0, 0, 0, 0, 0.01)',
+          '0px 45px 27px rgba(0, 0, 0, 0, 0.05)',
+          '0px 20px 20px rgba(0, 0, 0, 0, 0.09)',
+          '0px 5px 11px rgba(0, 0, 0, 0, 0.01)',
+          '0px 0px 0px rgba(0, 0, 0, 0, 0.01)',
+        ],
+        "xl-shadow" : [
+          '0px 41px 89px rgba(0, 0, 0, 0, 0.01)',
+          '0px 0px 0px rgba(0, 0, 0, 0, 0.01)',
+        ],
+      },
+    },
+  },
+  plugins: [],
+}
